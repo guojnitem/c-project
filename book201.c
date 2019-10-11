15e5eb278c66117e8bd158f8704d98b476130450
guojnitem <1250730121@qq.com>
2019-10-11 21:20:53 +0800

    commit file book201.c

---
+++ book201.c
@@ -0,0 +1,20 @@
+/*
+ * 输出一个*组成的H
+ * 作者：guojn
+*/
+#include<stdio.h>
+int main()
+{
+  fprintf(stdout,"输出一个H！\n");
+  printf("\t\t***\t***\n");   //一个\t是8格，linux下
+  printf("\t\t***\t***\n");
+  printf("\t\t***\t***\n");
+  printf("\t\t***\t***\n");
+  printf("\t\t***********\n");
+  printf("\t\t***********\n");
+  printf("\t\t***\t***\n");
+  printf("\t\t***\t***\n");
+  printf("\t\t***\t***\n");
+  printf("\t\t***\t***\n");
+  return 0;
+}
