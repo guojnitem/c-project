58686741cf93b91561b131e6d1d1c1ca54373b9b
guojnitem <1250730121@qq.com>
2019-10-15 23:28:58 +0800

    commit file book207.c

---
+++ book207.c
@@ -0,0 +1,17 @@
+/*
+ * 程序说明：循环演示
+ * 作者：guojn
+*/
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+  int i;
+  int sum=0;
+  for(i=0;i<=100;i++)
+  {
+    sum=sum+i;
+  }
+  printf("sum=%d\n",sum);
+}
